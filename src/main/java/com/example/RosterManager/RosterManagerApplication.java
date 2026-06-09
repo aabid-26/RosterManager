a0377ce96833c2be50ab2d58d13bcb34b5c60041
aabid-26 <aabidzimal@gmail.com>
2026-06-09 23:49:32 +0530

    Initial commit: Generate spring boot

---
+++ src/main/java/com/example/RosterManager/RosterManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.RosterManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RosterManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RosterManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RosterManager/RosterManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RosterManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RosterManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
